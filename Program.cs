0c646f19f1b1572b5c763385ad50ba3f04cd2ff7
DEV <dev@info.local>
2020-11-13 16:26:16 -0300

    Idade e semanas

--- Program.cs
+++ Program.cs
@@ -6,20 +6,18 @@ namespace C_Aula_2_Condicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva sua idade: ");
-            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva seu ano de nascimento: ");
+            int ano1 = int.Parse(Console.ReadLine());
 
-            int meses = (idade * 12);
-            int dias = (idade * 365);
-            int horas = (idade * 8760);
-            int minutos = (idade * 525600);
-            int nascimento = (2020 - idade);
+            Console.WriteLine("Escreva o ano atual: ");
+            int ano2 = int.Parse(Console.ReadLine());
+
+            int idade = (ano2 - ano1);
+            int semanas = (idade * 52);
+            
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Você nasceu em " + nascimento);
-            Console.WriteLine("Isso é igual a: " + meses + " meses");
-            Console.WriteLine("Isso é igual a: " + dias + " dias");
-            Console.WriteLine("Isso é igual a: " + horas + " horas");
-            Console.WriteLine("Isso é igual a: " + minutos + " minutos");
+            Console.WriteLine("Você tem " + idade + " anos de idade");
+            Console.WriteLine("Isso é igual a " + semanas + " semanas");
         }
     }
 }
