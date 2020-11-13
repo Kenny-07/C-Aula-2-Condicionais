6d7595abefa6892896ac707dd9c6c69ab02d43d6
DEV <dev@info.local>
2020-11-13 14:34:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Aula_2_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
