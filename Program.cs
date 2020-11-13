0dad6a8ab70d78ee4aac1da1428931256e81b289
DEV <dev@info.local>
2020-11-13 16:02:09 -0300

    teste pro valorant finalizado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace C_Aula_2_Condicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Bem vindo a seletiva para o time de Valorant");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Você gostaria de participar ?? (Sim ou Não)");
+            string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Que legal! Agora vamos analisar seu curriculo.");
+                    Console.WriteLine("Insira sua media escolar");
+                    int media = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a quantidade de faltas:");
+                    int faltas = int.Parse(Console.ReadLine());
+                    if(media >=50 && faltas <= 10) {
+                        Console.WriteLine("Parabens seja bem vindo =D");
+
+                    }else{
+                        Console.WriteLine("Que Pena, você precisa melhorar suas notas e faltas");
+                    }
+                }else{
+                    Console.WriteLine("Blz, passar bem");
+                }
         }
     }
 }
