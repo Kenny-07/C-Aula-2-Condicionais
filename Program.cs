f771956b378376cbc0f9af94dd5ffdd52053dd3f
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-15 23:00:05 -0300

    desafio nadadores

--- Program.cs
+++ Program.cs
@@ -2,21 +2,31 @@
 
 namespace C_Aula_2_Condicionais
 {
-    class Program
+     class Program
     {
+        private const char V = 'b';
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva seu salário: ");
-            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Olá nadador, digite aqui a sua idade em anos e nós iremos dizer em qual categoria você se encaixa.");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            int idade = int.Parse(Console.ReadLine());
 
-            double reajuste = ((salario * 0.3) + salario);
-            
-            if (salario >= 500){
-                Console.WriteLine("Você não pode receber o aumento");
+            Console.ForegroundColor = ConsoleColor.Green;
+            if(idade>=5 && idade<=7){
+                Console.WriteLine("Você é um competidor da categoria Infantil A. Boa Sorte!");
             }
-            else{
-                Console.WriteLine("Seu novo salário é de " + reajuste);
+            else if(idade>=8 && idade<=10){
+                Console.WriteLine("Você é um competidor da categoria Infantil B. Boa Sorte!");
             }
+            else if(idade>=11 && idade<=13){
+                Console.WriteLine("Você é um competidor da categoria Juvenil A. Boa Sorte!");
+            }
+            else if(idade>=14 && idade<=17){
+                Console.WriteLine("Você é um competidor da categoria Juveil B. Boa Sorte!");
+            }else{
+                Console.WriteLine("Você é um competidor da categoria Sênior. Boa Sorte!");
+            } 
         }
     }
 }
